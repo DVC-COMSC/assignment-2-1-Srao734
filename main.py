974b2aa2f1136ce615b10dbfe8092279fe8a82e1
Srao734 <136876420+Srao734@users.noreply.github.com>
2023-06-23 17:19:11 +0000

    female and male percentage code

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-num_males = input('Enter num males: ')
-num_females = input("Enter num females: ")
-sum = int(num_males) + int(num_females)
-m_perc = (int(num_males) / int(sum)) * 100
-f_perc = (int(num_females) / int(sum)) * 100
-print ("The total number of students: 		%d" % (sum))
-print ("The number of males and females: 	%s 	   %s" % (num_males, num_females))
-print (f'The percentage of males and females \t {m_perc:.2f} \t {f_perc:.2f}')
+def main():
 
+    num_males = input("Enter num males: ")
+    num_females = input("Enter num females: ")
+    sum = int(num_males) + int(num_females)
+    m_perc = (int(num_males) / int(sum)) * 100
+    f_perc = (int(num_females) / int(sum)) * 100
+    print ("The total number of students: 		%d" % (sum))
+    print ("The number of males and females: 	%s 	   %s" % (num_males, num_females))
+    print (f'The percentage of males and females \t {m_perc:.2f} \t {f_perc:.2f}')
 
+    return m_perc, f_perc
 
+main() 
